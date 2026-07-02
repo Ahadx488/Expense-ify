8b247d0faf8e193a9647fcc3ec3bcbfb5e2d1f62
Abdul Ahad Momin <bytewise58@gmail.com>
2026-07-02 10:52:10 +0530

    chore: configure environment variables for AWS S3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+//require("dotenv").config();
+require("dotenv").config({ path: "config.env" });
+
 
 const app = express();
 
